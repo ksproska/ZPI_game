df5c5bf7290fb9c40a7a106c1d0fbac0b2371d2b
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-09-28 21:02:51 +0200

    working drag-drop

--- ZPI_game/Assets/Scripts/DragDrop.cs
+++ ZPI_game/Assets/Scripts/DragDrop.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    private RectTransform _rectTransform;
+    private Canvas _canvas;
+    private CanvasGroup _canvasGroup;
+
+    private void Awake()
     {
-        
+        _rectTransform = GetComponent<RectTransform>();
+        _canvas = FindObjectOfType<Canvas>();
+        _canvasGroup = GetComponentInChildren<CanvasGroup>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnPointerDown(PointerEventData eventData) {}
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        _canvasGroup.alpha = .6f;
+        _canvasGroup.blocksRaycasts = false;
+    }
+    
+    public void OnDrag(PointerEventData eventData)
+    {
+        _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _canvasGroup.alpha = 1f;
+        _canvasGroup.blocksRaycasts = true;
     }
 }
