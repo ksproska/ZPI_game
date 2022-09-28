3de8b9526e578f82c44eb4fb3cbf841cf0ea26ea
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-09-28 21:06:09 +0200

    drag-drop - remove unnecessary

--- ZPI_game/Assets/Scripts/DragDrop.cs
+++ ZPI_game/Assets/Scripts/DragDrop.cs
@@ -5,31 +5,21 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 {
     private RectTransform _rectTransform;
     private Canvas _canvas;
-    private CanvasGroup _canvasGroup;
 
     private void Awake()
     {
         _rectTransform = GetComponent<RectTransform>();
         _canvas = FindObjectOfType<Canvas>();
-        _canvasGroup = GetComponentInChildren<CanvasGroup>();
     }
 
     public void OnPointerDown(PointerEventData eventData) {}
 
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        _canvasGroup.alpha = .6f;
-        _canvasGroup.blocksRaycasts = false;
-    }
+    public void OnBeginDrag(PointerEventData eventData) {}
     
     public void OnDrag(PointerEventData eventData)
     {
         _rectTransform.anchoredPosition += eventData.delta / _canvas.scaleFactor;
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        _canvasGroup.alpha = 1f;
-        _canvasGroup.blocksRaycasts = true;
-    }
+    public void OnEndDrag(PointerEventData eventData) {}
 }
