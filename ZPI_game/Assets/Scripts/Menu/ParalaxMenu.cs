a84c6f9d0c960e249838e0df45d1e3cdb5ebf9c4
Witold Frącek <witek.fracek@gmail.com>
2022-10-12 09:09:59 +0200

    modified main menu scene - added title

--- ZPI_game/Assets/Scripts/Menu/ParalaxMenu.cs
+++ ZPI_game/Assets/Scripts/Menu/ParalaxMenu.cs
@@ -16,7 +16,7 @@ public class ParalaxMenu : MonoBehaviour
 
     private void Update()
     {
-        position = -Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        position = Camera.main.ScreenToViewportPoint(Input.mousePosition);
         float posX = Mathf.Lerp(transform.position.x, startPosition.x + position.x * moveModifier, 2f * Time.deltaTime);
         float posY = Mathf.Lerp(transform.position.y, startPosition.y + position.y * moveModifier, 2f * Time.deltaTime);
         transform.position = new Vector3(posX, posY, 0f);
