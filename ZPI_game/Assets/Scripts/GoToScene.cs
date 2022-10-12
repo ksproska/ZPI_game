8d0d7b0911482dc4ba7967a2d0f08abbad91d702
Witold Frącek <witek.fracek@gmail.com>
2022-10-12 09:28:47 +0200

    Added sound to menu buttons

--- ZPI_game/Assets/Scripts/GoToScene.cs
+++ ZPI_game/Assets/Scripts/GoToScene.cs
@@ -1,11 +1,31 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GoToScene : MonoBehaviour
 {
     [SerializeField] public string scene;
+    [SerializeField] AudioClip clip;
     public void GoTo()
     {
         SceneManager.LoadScene(scene);
+    }
+
+    public void GoToAfterSound()
+    {
+        AudioSource audioSource = Camera.main.gameObject.GetComponent<AudioSource>();
+        if (audioSource != null)
+        {
+            StartCoroutine(PlaySoundAndGoTo(audioSource));
+            return;
+        }
+        GoTo();
+    }
+
+    IEnumerator PlaySoundAndGoTo(AudioSource source)
+    {
+        source.PlayOneShot(clip);
+        yield return new WaitWhile(() => source.isPlaying);
+        GoTo();
     }
 }
