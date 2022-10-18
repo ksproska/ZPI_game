c8bcfff9dc5ce03ece006d9a767d129ee3530c83
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-10-18 13:59:55 +0200

    mutation simulator - better looking

--- ZPI_game/Assets/Scripts/CreateIntsToDrop.cs
+++ ZPI_game/Assets/Scripts/CreateIntsToDrop.cs
@@ -6,20 +6,30 @@ using UnityEngine;
 public class CreateIntsToDrop : MonoBehaviour
 {
     [SerializeField] private GameObject toDropPrefab;
+    [SerializeField] private GameObject staticPrefab;
     [SerializeField] private GameObject dropSlot;
+
+    private List<GameObject> allCreatedSlots = new List<GameObject>();
     public void Start()
     {
         for (int i = 0; i < 10; i++)
         {
+            var slot = Instantiate(dropSlot, gameObject.transform.position, Quaternion.identity,
+                FindObjectOfType<Canvas>().transform);
+            slot.transform.position += Vector3.down * 100;
+            slot.transform.position += Vector3.right * i * 100;
+            allCreatedSlots.Add(slot);
+            
+            var staticAdded = Instantiate(staticPrefab, gameObject.transform.position, Quaternion.identity,
+                FindObjectOfType<Canvas>().transform);
+            var staticDd = staticAdded.GetComponent<TextMeshProUGUI>();
+            staticDd.text = $"{i}";
+            staticAdded.transform.position += Vector3.right * i * 100;
+            
             var added = Instantiate(toDropPrefab, gameObject.transform.position, Quaternion.identity,
                 FindObjectOfType<Canvas>().transform);
             var dd = added.GetComponent<DragDrop>();
             dd.SetContent($"{i}");
-            added.transform.position += Vector3.right * i * 100;
-                
-            added = Instantiate(dropSlot, gameObject.transform.position, Quaternion.identity,
-                FindObjectOfType<Canvas>().transform);
-            added.transform.position += Vector3.down * 100;
             added.transform.position += Vector3.right * i * 100;
         }
     }
