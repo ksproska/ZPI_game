9f28c8a83de702ab9c49bfe0ec8d7fdf7b6010f6
Kuba Jastrzebski <kubixxu@gmail.com>
2022-10-20 11:57:27 +0200

    Added class for saving current game state

---
+++ ZPI_game/Assets/CurrentState/CurrentGameState.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LevelUtils;
+
+namespace CurrentState
+{
+    public static class CurrentGameState
+    {
+        public static LoadSaveHelper.SlotNum CurrentSlot { get; set; }
+        public static string CurrentLevelName { get; set; }
+    }
+}
