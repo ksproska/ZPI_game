994c5e336a3a0cd14dd6661ad4bbcefbca1f5894
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-10-22 20:33:11 +0200

    new text files for gaps and descriptions + automatic code update

---
+++ ZPI_game/Assets/Scripts/Education/AddTextWithGapsFromFile.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class AddTextWithGapsFromFile : MonoBehaviour
+{
+    [SerializeField] private string filename;
+    void Start()
+    {
+        string path = Directory.GetCurrentDirectory() + $"\\Assets\\PythonTexts\\{filename}.txt";
+        string readText = File.ReadAllText(path);
+        var textmesh = gameObject.GetComponent<TextMeshProUGUI>();
+        EditorUtility.SetDirty(textmesh);
+        textmesh.text = readText;
+    }
+}
