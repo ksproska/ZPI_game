a07626a724563b3633d904fddf7923cae67d126d
Kamila Sproska <61067969+ksproska@users.noreply.github.com>
2022-10-22 22:19:57 +0200

    new details for education display

--- ZPI_game/Assets/Scripts/Education/TextLinkHandler.cs
+++ ZPI_game/Assets/Scripts/Education/TextLinkHandler.cs
@@ -6,25 +6,27 @@ using System.IO;
 
 public class TextLinkHandler : MonoBehaviour, IPointerClickHandler
 {
-    [SerializeField] private TextMeshProUGUI details;
+    private TextMeshProUGUI details;
+    private ShowOrHide _showOrHide;
+
+    public void Start()
+    {
+        _showOrHide = GameObject.FindWithTag("CodeDetails").GetComponent<ShowOrHide>();
+        details = GameObject.FindWithTag("CodeDetails").GetComponentInChildren<TextMeshProUGUI>();
+        _showOrHide.Hide();
+    }
 
     public void OnPointerClick(PointerEventData eventData) {
         var pTextMeshPro = GetComponent<TextMeshProUGUI>();
         int linkIndex = TMP_TextUtilities.FindIntersectingLink(pTextMeshPro, eventData.position, null);
+        
         if (linkIndex != -1) { // was a link clicked?
+            _showOrHide.Show();
             TMP_LinkInfo linkInfo = pTextMeshPro.textInfo.linkInfo[linkIndex];
             details.text = "Details for <b>" + linkInfo.GetLinkText() + "</b>:\n";
-            if (linkInfo.GetLinkID() == "") {
-                string path = Directory.GetCurrentDirectory() + "\\Assets\\DescriptionTexts\\" + linkInfo.GetLinkText() + ".txt";
-                string readText = File.ReadAllText(path);
-                details.text += "> " +readText;
-            }
-            else
-            {
-                string path = Directory.GetCurrentDirectory() + "\\Assets\\DescriptionTexts\\GA\\" + linkInfo.GetLinkID() + ".txt";
-                string readText = File.ReadAllText(path);
-                details.text += readText;
-            }
+            string path = Directory.GetCurrentDirectory() + "\\Assets\\DescriptionTexts\\" + linkInfo.GetLinkID() + ".txt";
+            string readText = File.ReadAllText(path);
+            details.text += readText;
         }
     }
 }
