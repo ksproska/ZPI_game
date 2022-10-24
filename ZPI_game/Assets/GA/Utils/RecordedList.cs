d122974bc40993fe9703382bdb3bfed56f035b76
Witold Frącek <witek.fracek@gmail.com>
2022-10-24 12:32:17 +0200

    init files for RecordedList

--- ZPI_game/Assets/GA/Utils/RecordedList.cs
+++ ZPI_game/Assets/GA/Utils/RecordedList.cs
@@ -8,6 +8,34 @@ namespace Assets.GA.Utils
 {
     class RecordedList<T>
     {
+        private List<List<T>> history;
+        private List<T> currentState;
 
+        public RecordedList()
+        {
+            currentState = new List<T>();
+            history = new List<List<T>>();
+            history.Add(currentState);
+        }
+
+        public RecordedList(IEnumerable<T> args)
+        {
+            currentState = args.ToList();
+            history = new List<List<T>>();
+            history.Add(currentState);
+        }
+
+        public T this[int index]
+        {
+            set => SetElementAt(index, value);
+            get => currentState[index];
+        }
+
+        private void SetElementAt(int index, T element)
+        {
+            currentState[index] = element;
+            var newState = new List<T>(currentState);
+            history.Add(newState);
+        }
     }
 }
