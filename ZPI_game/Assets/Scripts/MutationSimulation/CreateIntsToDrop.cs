0795f9b8640f9fd2a4ca88ea74ba7a68436d8378
Maciej Futyma <mfutyma2000@gmail.com>
2022-10-24 21:43:32 +0200

    Glitch occurence dependent of number of completed levels

--- ZPI_game/Assets/Scripts/MutationSimulation/CreateIntsToDrop.cs
+++ ZPI_game/Assets/Scripts/MutationSimulation/CreateIntsToDrop.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CreateIntsToDrop : MonoBehaviour
 {
@@ -12,25 +13,64 @@ public class CreateIntsToDrop : MonoBehaviour
     private List<GameObject> allCreatedSlots = new List<GameObject>();
     public void Start()
     {
+        for (int i = 0; i < 3; i++)
+        {
+            var slot = Instantiate(dropSlot, gameObject.transform.position, Quaternion.identity,
+                FindObjectOfType<Canvas>().transform);
+            slot.transform.position += new Vector3(500 + i * 120, -120, 0);
+            DropSlot ds = slot.GetComponent<DropSlot>();
+            ds.expectedContents = $"{i}";
+            slot.layer = 0;
+            allCreatedSlots.Add(slot);
+
+            var staticAdded = Instantiate(staticPrefab, gameObject.transform.position, Quaternion.identity,
+                FindObjectOfType<Canvas>().transform);
+            var staticDd = staticAdded.GetComponent<TextMeshProUGUI>();
+            staticDd.text = $"{i}";
+            staticAdded.transform.position += new Vector3(500 + i * 120, 0, 0);
+            staticAdded.layer = 0;
+
+            var added = Instantiate(toDropPrefab, gameObject.transform.position, Quaternion.identity,
+                FindObjectOfType<Canvas>().transform);
+            var dd = added.GetComponent<DragDrop>();
+            dd.SetContent($"{i}");
+            added.transform.position += new Vector3(500 + i * 120, 0, 0);
+            added.layer = 1;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+
+            var added = Instantiate(toDropPrefab, gameObject.transform.position, Quaternion.identity,
+                FindObjectOfType<Canvas>().transform);
+            var dd = added.GetComponent<DragDrop>();
+            dd.SetContent($"{i}");
+            added.transform.position += new Vector3(500 + i * 120, 0, 0);
+            added.layer = 1;
+        }
+    }
+
+
+    public void CreateTutorial()
+    {
         for (int i = 0; i < 10; i++)
         {
             var slot = Instantiate(dropSlot, gameObject.transform.position, Quaternion.identity,
                 FindObjectOfType<Canvas>().transform);
             slot.transform.position += Vector3.down * 100;
-            slot.transform.position += Vector3.right * i * 100;
+            slot.transform.position += Vector3.right * i * 100 + new Vector3(500, 0, 0);
             allCreatedSlots.Add(slot);
-            
+
             var staticAdded = Instantiate(staticPrefab, gameObject.transform.position, Quaternion.identity,
                 FindObjectOfType<Canvas>().transform);
             var staticDd = staticAdded.GetComponent<TextMeshProUGUI>();
             staticDd.text = $"{i}";
-            staticAdded.transform.position += Vector3.right * i * 100;
-            
+            staticAdded.transform.position += Vector3.right * i * 100 + new Vector3(500, 0, 0);
+
             var added = Instantiate(toDropPrefab, gameObject.transform.position, Quaternion.identity,
                 FindObjectOfType<Canvas>().transform);
             var dd = added.GetComponent<DragDrop>();
             dd.SetContent($"{i}");
-            added.transform.position += Vector3.right * i * 100;
+            added.transform.position += Vector3.right * i * 100 + new Vector3(500, 0, 0);
         }
     }
 }
--- ZPI_game/Assets/Scripts/WorldMapHandler.cs
+++ ZPI_game/Assets/Scripts/WorldMapHandler.cs
@@ -3,35 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using CurrentState;
+using LevelUtils;
+using System.Linq;
+using System;
 
 public class WorldMapHandler : MonoBehaviour
 {
-    private float nextTime;
-    private float modifier;
+    private float prevTime;
+    private float glitchRandom;
     public Image map;
     public Sprite glithched;
+    public int glitchBeginningLevel;
     public GameObject wires;
     [SerializeField] private AudioSource source;
+    private int glitchLevel;
+    private float glithprobability;
     // Start is called before the first frame update
     void Start()
     {
-        modifier = Random.Range(5.0f, 10.0f);
-        nextTime = Time.time + modifier;
+        glitchLevel = Math.Max(0,(LevelMap.GetListOfLevels(CurrentGameState.CurrentSlot).Max(level => level.LevelNumber)) - glitchBeginningLevel);
+        prevTime = Time.time;
         source.volume = CurrentGameState.EffectsVolume;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Time.time > nextTime)
+        glitchRandom = UnityEngine.Random.Range(0.0f, 1.0f);
+
+        if (Time.time - prevTime > 1.0f )
         {
-
-            modifier = Random.Range(5.0f, 100.0f);
-            nextTime = Time.time + modifier;
-
-            StartCoroutine(glitch());
-
-
+            if(glitchRandom < glitchLevel * 0.01f)
+            {
+                StartCoroutine(glitch());
+            }
+            prevTime = Time.time;
         }
     }
 
@@ -39,40 +45,23 @@ public class WorldMapHandler : MonoBehaviour
     {
         source.Play();
         Sprite notGlithched = map.sprite;
+
         map.sprite = glithched;
-        foreach(var wire in wires.GetComponentsInChildren<SpriteRenderer>())
-        {
-            wire.enabled = false;
-        }
+        wires.SetActive(false);
         yield return new WaitForSeconds(0.05f);
         map.sprite = notGlithched;
-        foreach (var wire in wires.GetComponentsInChildren<SpriteRenderer>())
-        {
-            wire.enabled = true;
-        }
+        wires.SetActive(true);
         yield return new WaitForSeconds(0.1f);
         map.sprite = glithched;
-        foreach (var wire in wires.GetComponentsInChildren<SpriteRenderer>())
-        {
-            wire.enabled = false;
-        }
+        wires.SetActive(false);
         yield return new WaitForSeconds(0.01f);
         map.sprite = notGlithched;
-        foreach (var wire in wires.GetComponentsInChildren<SpriteRenderer>())
-        {
-            wire.enabled = true;
-        }
+        wires.SetActive(true);
         yield return new WaitForSeconds(0.1f);
         map.sprite = glithched;
-        foreach (var wire in wires.GetComponentsInChildren<SpriteRenderer>())
-        {
-            wire.enabled = false;
-        }
+        wires.SetActive(false);
         yield return new WaitForSeconds(0.25f);
         map.sprite = notGlithched;
-        foreach (var wire in wires.GetComponentsInChildren<SpriteRenderer>())
-        {
-            wire.enabled = true;
-        }
+        wires.SetActive(true);
     }
 }
