37db48ecb299faecb37a1963571362234533d821
Maciej Futyma <mfutyma2000@gmail.com>
2022-10-25 13:41:36 +0200

    switching between tutorial and user task

--- ZPI_game/Assets/Scripts/MutationSimulation/TutorialController.cs
+++ ZPI_game/Assets/Scripts/MutationSimulation/TutorialController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,7 +8,18 @@ public class TutorialController : MonoBehaviour
 {
 
     public DropHandler dropHandler;
+
+    public GameObject levelButton;
     public GameObject tutorialButton;
+    public GameObject checkButton;
+
+    public CreateIntsToDrop gridCreator;
+
+    public GameObject tutorialCanvas;
+    public GameObject levelCanvas;
+
+    public GameObject nextButton;
+    public GameObject previousButton;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,5 +38,35 @@ public class TutorialController : MonoBehaviour
         {
             tutorialButton.SetActive(true);
         }
+    }
+
+
+
+    public void CreateTutorial()
+    {
+
+        levelCanvas.SetActive(false);
+        tutorialCanvas.SetActive(true);
+        gridCreator.CreateTutorial(tutorialCanvas);
+        tutorialButton.SetActive(false);
+        checkButton.SetActive(false);
+        levelButton.SetActive(true);
+        nextButton.SetActive(true);
+        previousButton.SetActive(true);
+    }
+
+    public void LeaveTutorial()
+    {
+        gridCreator.DestroyTutorial();
+        //GameObject.Find("LevelCanvas").SetActive(true);
+        //GameObject.Find("TutorialCanvas").SetActive(false);
+        levelCanvas.SetActive(true);
+        tutorialCanvas.SetActive(false);
+        tutorialButton.SetActive(true);
+        checkButton.SetActive(true);
+        levelButton.SetActive(false);
+        nextButton.SetActive(false);
+        previousButton.SetActive(false);
+
     }
 }
