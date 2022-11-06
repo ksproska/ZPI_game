e3aa18dbd2777030ad85172c161023e0011eab85
Kuba Jastrzebski <kubixxu@gmail.com>
2022-11-06 11:56:35 +0100

    Added new json schema for Map class

--- ZPI - webserver/flask-app/data_model_schemas.py
+++ ZPI - webserver/flask-app/data_model_schemas.py
@@ -1,4 +1,5 @@
 from flask_marshmallow import Marshmallow
+from marshmallow import fields
 
 fl_marsh = Marshmallow()
 
@@ -6,9 +7,20 @@ class PointSchema(fl_marsh.Schema):
     class Meta:
         fields = ('X', 'Y')
 
+class MapSchema(fl_marsh.Schema):
+    class Meta:
+        strict = True
+    
+    MapId = fields.Integer(attribute='map_id')
+    CreatorId = fields.Integer(attribute='user_id')
+    CreationDate = fields.Date(attribute='creation_date')
+    Points = fl_marsh.Nested(PointSchema, many=True, attribute='points')
+
 class Schemas():
     point_schema = PointSchema()
     points_schema = PointSchema(many=True)
+    map_schema = MapSchema()
+    maps_schema = MapSchema(many=True)
 
 def get_marshmallow():
     return fl_marsh
