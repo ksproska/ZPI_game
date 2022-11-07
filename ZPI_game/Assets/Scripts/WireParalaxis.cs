2a3928b2cdffb3244c39224da4c143adab847351
Maciej Futyma <mfutyma2000@gmail.com>
2022-11-07 17:06:35 +0100

    renaming camera object

--- ZPI_game/Assets/Scripts/WireParalaxis.cs
+++ ZPI_game/Assets/Scripts/WireParalaxis.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 public class WireParalaxis : MonoBehaviour
 {
     private float cameraStartPosition;
-    public GameObject camera;
+    public GameObject mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        cameraStartPosition = camera.transform.position.y;
+        cameraStartPosition = mainCamera.transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float deltaPosition = cameraStartPosition - camera.transform.position.y;
-        cameraStartPosition = camera.transform.position.y;
+        float deltaPosition = cameraStartPosition - mainCamera.transform.position.y;
+        cameraStartPosition = mainCamera.transform.position.y;
         transform.position += new Vector3 (0, deltaPosition * 0.3f);
     }
 }
