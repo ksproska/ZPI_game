c3f304620eeda5a8b5c2eddb3c745807215319e5
Witold Frącek <witek.fracek@gmail.com>
2022-11-07 12:43:41 +0100

    Basic login screen added

---
+++ ZPI_game/Assets/Scripts/Menu/Account/LoginValidator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.Menu.Account
+{
+    class LoginValidator: MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI loginText;
+        [SerializeField] private Text loginValidationText;
+
+        [SerializeField] private TextMeshProUGUI passwordText;
+        [SerializeField] private Text passwordValidationText;
+
+        [SerializeField] private GameObject connectionInfoFrame;
+
+        private Regex emailRegex;
+
+
+        private void Start()
+        {
+            emailRegex = new(@"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*
+                             @((([\-\w]+\.)+[a-zA-Z]{2,4})|(([0-9]{1,3}\.){3}[0-9]{1,3}))$");
+        }
+
+        public void SendData()
+        {
+            var login = loginText.text;
+            var password = passwordText.text;
+            // SomeController.SendData(login, password);
+            bool hasInternetConnection = false;
+            if(!hasInternetConnection)
+            {
+                connectionInfoFrame.SetActive(true);
+            }
+        }
+
+    }
+
+
+}
---
+++ ZPI_game/Assets/Scripts/Menu/CryoHoverBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Assets.Cryo.Script;
+
+namespace Assets.Scripts.Menu
+{
+    class CryoHoverBehaviour : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField] private CryoUI cryo;
+        [SerializeField] private EyeType eyesNormalState;
+        [SerializeField] private EyeType eyesTriggeredState;
+        [SerializeField] private MouthType mouthNormalState;
+        [SerializeField] private MouthType mouthTriggeredState;
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            cryo.SetBothEyesTypes(eyesTriggeredState);
+            cryo.SetMouthType(mouthTriggeredState);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            cryo.SetBothEyesTypes(eyesNormalState);
+            cryo.SetMouthType(mouthNormalState);
+        }
+    }
+}
