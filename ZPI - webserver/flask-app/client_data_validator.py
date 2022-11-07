3c5ca6779d70a0421b10b8aa2416c4ce0848710d
Kuba Jastrzebski <kubixxu@gmail.com>
2022-11-07 17:31:15 +0100

    Added auth methods on server side

--- ZPI - webserver/flask-app/client_data_validator.py
+++ ZPI - webserver/flask-app/client_data_validator.py
@@ -2,6 +2,7 @@ import marshmallow.exceptions as ex
 import re
 
 class ClientDataValidator():
+
     @staticmethod
     def validate_user(usr_serialized: dict):
 
@@ -12,7 +13,7 @@ class ClientDataValidator():
         elif not 'Password' in usr_serialized:
             raise ex.ValidationError(f'Client received data has no key \'Password\'!')
 
-        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}\b'
         if not re.fullmatch(regex, usr_serialized['Email']):
             raise ex.ValidationError('Email address has a wrong format!')
         elif len(usr_serialized['Email']) > 200:
@@ -23,3 +24,22 @@ class ClientDataValidator():
         
         if len(usr_serialized['Nickname']) > 30:
             raise ex.ValidationError('Nickname can be maximally 30 characters long!')
+        elif len(usr_serialized['Nickname']) < 3:
+            raise ex.ValidationError('Nickname has to be at least 3 characters long!')
+    
+    @staticmethod
+    def validate_user_creds(user_creds_serialized: dict):
+
+        if not 'Email' in user_creds_serialized:
+            raise ex.ValidationError(f'Client received data has no key \'Email\'!')
+        elif not 'Password' in user_creds_serialized:
+            raise ex.ValidationError(f'Client received data has no key \'Password\'!')
+
+        regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Z|a-z]{2,}\b'
+        if not re.fullmatch(regex, user_creds_serialized['Email']):
+            raise ex.ValidationError('Email address has a wrong format!')
+        elif len(user_creds_serialized['Email']) > 200:
+            raise ex.ValidationError('Email address too long!')
+        
+        if len(user_creds_serialized['Password']) < 12:
+            raise ex.ValidationError('Password should be at least 12 characters long!')
--- ZPI - webserver/flask-app/routes.py
+++ ZPI - webserver/flask-app/routes.py
@@ -6,6 +6,9 @@ from db_models import Users
 from db_queries import ReadQueries, CreateQueries
 from client_data_validator import ClientDataValidator
 from database_validator import DatabaseValidator
+from hashlib import sha256
+from hmac import compare_digest
+
 
 routes = Blueprint('routes', __name__)
 
@@ -58,6 +61,24 @@ def create_user():
         return f'{i_err.statement}', 409
     usr = CreateQueries.create_user(usr)
     return 'User created', 200
+
+@routes.route('/api/auth', methods=['POST'])
+def authenticate_user():
+    user_creds_serialized = request.json
+    try:
+        ClientDataValidator.validate_user_creds(user_creds_serialized)
+    except ValidationError as v_err:
+        return f'{v_err.messages[0]}', 409
+    usr = ReadQueries.get_user(user_creds_serialized['Email'])
+    if usr is None:
+        return 'User with this email address and password couldn\'t be found!', 401
+    sha_engine = sha256()
+    sha_engine.update(user_creds_serialized['Password'].encode(encoding='utf-8'))
+    pass_sha = sha_engine.hexdigest()
+    if not compare_digest(pass_sha, usr.password):
+        return 'User with this email address and password couldn\'t be found!', 401
+
+    return jsonify(Schemas.user_schema.dump(usr)), 200
 
 
 def get_routes():
