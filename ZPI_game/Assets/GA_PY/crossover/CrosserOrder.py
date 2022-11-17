0f9cf7a76011ea0c502a3dc7440597bec12d7626
Kamila Sproska <kamila.sproska@gmail.com>
2022-11-17 12:20:59 +0100

    update for code with gaps - small changes

--- ZPI_game/Assets/GA_PY/crossover/CrosserOrder.py
+++ ZPI_game/Assets/GA_PY/crossover/CrosserOrder.py
@@ -3,7 +3,8 @@ import random
 
 class CrosserOrder:
     @staticmethod
-    def cross(parent1: list[int], parent2: list[int], start_index: int, segment_length: int):
+    def cross(parent1: list[int], parent2: list[int],
+              start_index: int, segment_length: int):
         """
         Method returns a child for given parents and length of a segment.
         """
@@ -15,11 +16,14 @@ class CrosserOrder:
         last_not_contained = 0  # 0
         for i in range(len(child)):
             if child[i] is None:  # None
-                last_not_contained = CrosserOrder.__put_next(child, parent2, i, last_not_contained)
+                last_not_contained = CrosserOrder.__put_next(
+                    child, parent2, i, last_not_contained
+                )
         return child
 
     @staticmethod
-    def __put_next(child: list[int], parent2: list[int], curr_i: int, last_not_contained: int):
+    def __put_next(child: list[int], parent2: list[int],
+                   curr_i: int, last_not_contained: int):
         for j in range(last_not_contained, len(parent2)):
             if parent2[j] not in child:  # not
                 child[curr_i] = parent2[j]  # parent2
@@ -32,5 +36,4 @@ class CrosserOrder:
         """
         start_inx = random.randrange(len(parent1))  # randrange
         segment_len = random.randrange(max(len(parent1) - start_inx - 1, 1))  # randrange
-
         return self.cross(parent1, parent2, start_inx, segment_len)
