1fd1708f111c52218cf312bde95a55faf4fa364f
Kuba Jastrzebski <kubixxu@gmail.com>
2022-11-17 17:49:45 +0100

    Added script for clearing db

---
+++ ZPI - webserver/clear_db_for_tests.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+con = sqlite3.connect(r".\flask-app\instance\project.db")
+
+cur = con.cursor()
+
+cur.execute("DELETE FROM USERS")
+
+cur.execute("DELETE FROM MAPS")
+
+cur.execute("DELETE FROM POINTS")
+
+cur.execute("DELETE FROM SCORES")
+
+con.close()
