53646f23b3992b75f723f8e6de9eeb7657db6666
Kamila Sproska <kamila.sproska@gmail.com>
2022-11-22 16:27:53 +0100

    adding descriptions to methods part 1

--- ZPI_game/Assets/GA_PY/crossover/CrosserPartiallyMatched.py
+++ ZPI_game/Assets/GA_PY/crossover/CrosserPartiallyMatched.py
@@ -5,8 +5,22 @@ class CrosserPartiallyMatched:
     @staticmethod
     def cross(parent1: list[int], parent2: list[int],
               start_index: int, segment_length: int):
-        """
-        Method returns a child for given parents, start index, and length of a segment.
+        """Method returns a child for given parents, start index, and length of a segment, example:
+        parent1 = [8, 4, 7, 3, 6, 2, 5, 1, 9, 0]
+        parent2 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+        start_index = 3
+        segment_length = 5
+
+        child:    [0, 7, 4, 3, 6, 2, 5, 1, 8, 9] 
+
+        Args:
+            parent1 (list[int]): genotype of the first parent
+            parent2 (list[int]): genotype of the second parent
+            start_index (int): index of the first element of the chosen segment
+            segment_length (int): length of the element of the chosen segment
+
+        Returns:
+            list[int]: genotype of the child
         """
         child = [None for _ in range(len(parent1))]  # None
         for i in range(start_index, segment_length):
@@ -27,8 +41,14 @@ class CrosserPartiallyMatched:
         return child
 
     def get(self, parent1: list[int], parent2: list[int]):
-        """
-        Method returns a child for given parents. Start index and length of a segment are drawnded.
+        """Method returns a child for given parents. Length of a segment to be copied is drawned.
+
+        Args:
+            parent1 (list[int]): genotype of the first parent
+            parent2 (list[int]): genotype of the second parent
+
+        Returns:
+            list[int]: genotype of the child
         """
         start_inx = random.randrange(len(parent1) - 1)  # randrange
         segment_len = random.randrange(len(parent1) - start_inx - 1)
--- ZPI_game/Assets/GA_PY/mutation/MutatorReverseSequence.py
+++ ZPI_game/Assets/GA_PY/mutation/MutatorReverseSequence.py
@@ -6,6 +6,22 @@ class MutatorReverseSequence:
 
     @staticmethod
     def mutate(city_ids: list[int], start_index: int, end_index: int) -> list[int]:
+        """Returns child's mutated genotype, example:
+        child =  [0, 1, 2, 3, 4, 5]
+        start_index = 1
+        end_index = 4
+
+        mutated: [0, 4, 3, 2, 1, 5]
+
+        ---------------------------------------------------------
+        Args:
+            city_ids (list[int]): child's genotype
+            start_index (int): first index of the segment
+            end_index (int): last index of the segment
+
+        Returns:
+            list[int]: mutated child's genotype
+        """
         city_ids_copy = deepcopy(city_ids)  # deepcopy
         length = end_index - start_index + 1 \
             if end_index < start_index \
@@ -19,6 +35,14 @@ class MutatorReverseSequence:
         return city_ids_copy
 
     def get(self, city_ids: list[int]) -> list[int]:
+        """Returns child's mutated genotype. The start and end indexes are randomly selected here.
+
+        Args:
+            city_ids (list[int]): child's genotype
+
+        Returns:
+            list[int]: mutated child's genotype
+        """
         start_index = randrange(0, len(city_ids))  # randrange
         end_index = randrange(0, len(city_ids))
         return self.mutate(city_ids, start_index, end_index)
