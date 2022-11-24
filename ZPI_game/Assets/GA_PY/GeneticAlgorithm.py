656cee8edc3a2766afa565f86c43f83069685a4d
Maciej Futyma <mfutyma2000@gmail.com>
2022-11-24 18:21:10 +0100

    renamed too long variables

--- ZPI_game/Assets/GA_PY/GeneticAlgorithm.py
+++ ZPI_game/Assets/GA_PY/GeneticAlgorithm.py
@@ -6,17 +6,18 @@ from Circuit import Circuit
 class GeneticAlgorithm:
 
     def __init__(self, weights_grid: DistancesGrid, generation_size: int, selector,
-                 mutator, mutation_probability: float,
-                 crosser, crossover_probability: float):
+                 mutator, mut_prob: float,
+                 crosser, cross_prob: float):
 
         self.weights_grid = weights_grid
         self.selector = selector
         self.mutator = mutator  # mutator
-        self.mutation_probability = mutation_probability  # mutation_probability
+        self.mut_prob = mut_prob  # mut_prob
         self.crosser = crosser
-        self.crossover_probability = crossover_probability
+        self.cross_prob = cross_prob
 
-        self.generation = [Circuit.get_random(weights_grid) for _ in range(generation_size)]
+        self.generation = [Circuit.get_random(
+            weights_grid) for _ in range(generation_size)]
         self.best = Circuit.get_best(self.generation)
         self.best_for_iteration = self.best
         self.iteration = 0
@@ -36,11 +37,13 @@ class GeneticAlgorithm:
             parent2 = self.selector.get(self.generation)
 
             child_genotype = parent1.city_ids
-            if self.crossover_probability < random.random():
-                child_genotype = self.crosser.get(parent1.city_ids, parent2.city_ids)
+            if self.cross_prob < random.random():
+                child_genotype = self.crosser.get(
+                    parent1.city_ids, parent2.city_ids)
 
-            if self.mutation_probability < random.random():  # mutation_probability
-                child_genotype = self.mutator.get(child_genotype)  # mutator child_genotype
+            if self.mut_prob < random.random():  # mut_prob
+                child_genotype = self.mutator.get(
+                    child_genotype)  # mutator child_genotype
 
             child = Circuit(child_genotype, self.weights_grid)
             next_generation.append(child)
--- ZPI_game/Assets/GA_PY/GeneticAlgorithm_2.py
+++ ZPI_game/Assets/GA_PY/GeneticAlgorithm_2.py
@@ -5,10 +5,10 @@ from Circuit import Circuit
 
 class GeneticAlgorithm:
     def __init__(self, weights_grid: DistancesGrid, generation_size: int, selector,
-                 crosser, crossover_probability: float):
+                 crosser, cross_prob: float):
         ...
         self.crosser = crosser  # crosser
-        self.crossover_probability = crossover_probability  # crossover_probability
+        self.cross_prob = cross_prob  # cross_prob
         ...
 
     def run_iteration(self):
@@ -18,22 +18,22 @@ class GeneticAlgorithm:
         2. Cross two parents resulting in a new child (which might get "the best genes" from both parents)
         ...
         """
-        next_generation = list()
-        while len(next_generation) < len(self.generation):
+        next_gen = list()
+        while len(next_gen) < len(self.generation):
             parent1 = self.selector.get(self.generation)
             parent2 = self.selector.get(self.generation)
 
             child_genotype = parent1.city_ids  # city_ids
-            if self.crossover_probability < random.random():  # crossover_probability
+            if self.cross_prob < random.random():  # cross_prob
                 child_genotype = self.crosser.get(
                     parent1.city_ids, parent2.city_ids)  # crosser city_ids
             ...
 
             child = Circuit(child_genotype, self.weights_grid)  # Circuit child_genotype
-            next_generation.append(child)  # append
-        self.generation = next_generation  # next_generation
-        self.best_for_iteration = Circuit.get_best(self.generation)
+            next_gen.append(child)  # append
+        self.generation = next_gen  # next_gen
+        self.best_for_iter = Circuit.get_best(self.generation)
         self.iteration += 1  # +=
 
-        if self.best.score > self.best_for_iteration.score:  # score
-            self.best = self.best_for_iteration  # best_for_iteration
+        if self.best.score > self.best_for_iter.score:  # score
+            self.best = self.best_for_iter  # best_for_iter
