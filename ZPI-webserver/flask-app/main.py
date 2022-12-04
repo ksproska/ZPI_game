a88f0e3463b79ec4fe5a20a8fd25260daf52e451
Kuba Jastrzebski <kubixxu@gmail.com>
2022-12-04 08:55:03 +0100

    Added changes for web server testing

--- ZPI-webserver/flask-app/main.py
+++ ZPI-webserver/flask-app/main.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from flask_talisman import Talisman
 from db_models import get_db
 from routes import get_routes
 from data_model_schemas import get_marshmallow
 
 app = Flask(__name__)
-Talisman(app, content_security_policy=None)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 db = get_db()
 db.init_app(app)
@@ -18,7 +16,6 @@ fl_marshmallow.init_app(app)
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
-    context = ('server.crt', 'server.key')    
-    app.run(host='0.0.0.0', port=5000, ssl_context=context)
+        db.create_all() 
+    app.run(host='0.0.0.0', port=5000)
 
