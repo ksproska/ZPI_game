105277eaafccf47abd3dafb1c43ef1d8d1ee707e
Maciej Futyma <mfutyma2000@gmail.com>
2022-12-11 12:50:34 +0100

    Fixed credits scroller

--- ZPI_game/Assets/Scripts/Menu/Credits/CreditsScroller.cs
+++ ZPI_game/Assets/Scripts/Menu/Credits/CreditsScroller.cs
@@ -11,15 +11,16 @@ public class CreditsScroller : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI credits;
     [SerializeField] private float speed;
+    [SerializeField] private RectTransform window;
+
     private Vector3 startPosition;
     private float windowSize;
     private float startSize;
     private string creditsText = "EMPTY";
-    private float time = 2;
 
     private void Awake()
     {
-        windowSize = GetComponent<RectTransform>().rect.height;
+        windowSize =window.rect.height;
         startPosition = GetComponent<RectTransform>().anchoredPosition3D;
         creditsText = Resources.Load<TextAsset>("Credits/credits").text;
         credits.text = creditsText;
@@ -31,7 +32,6 @@ public class CreditsScroller : MonoBehaviour
 
     private void OnEnable()
     {
-        time = 2;
         Canvas.ForceUpdateCanvases();
         startSize = credits.gameObject.GetComponent<RectTransform>().rect.height;
         GetComponent<RectTransform>().anchoredPosition3D = startPosition + Vector3.down * (startSize / 2 + windowSize/2);
@@ -40,19 +40,12 @@ public class CreditsScroller : MonoBehaviour
 
     private void Update()
     {
-        if (time > 0)
+        startSize = credits.gameObject.GetComponent<RectTransform>().rect.height;
+        float distance = speed * Time.deltaTime;
+        GetComponent<RectTransform>().anchoredPosition3D += new Vector3(0, distance, 0);
+        if (GetComponent<RectTransform>().anchoredPosition3D.y > startPosition.y + startSize / 2 + windowSize / 2)
         {
-            time -= Time.deltaTime;
-        }
-        else
-        {  
-            startSize = credits.gameObject.GetComponent<RectTransform>().rect.height;
-            float distance = speed * Time.deltaTime;
-            GetComponent<RectTransform>().anchoredPosition3D += new Vector3(0, distance, 0);
-            if (GetComponent<RectTransform>().anchoredPosition3D.y > startPosition.y + startSize / 2 + windowSize / 2)
-            {
-                GetComponent<RectTransform>().anchoredPosition3D = startPosition + Vector3.down * (startSize / 2 + windowSize / 2);
-            }
+            GetComponent<RectTransform>().anchoredPosition3D = startPosition + Vector3.down * (startSize / 2 + windowSize / 2);
         }
 
     }
